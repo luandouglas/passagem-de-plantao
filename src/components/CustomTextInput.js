02b901fd2cee6e9e4e04ffec1874d5bdf4eabd35
josileudo <48743781+josileudo@users.noreply.github.com>
2020-07-20 17:50:22 -0300

    TextInput Mask added in project

--- src/components/CustomTextInput.js
+++ src/components/CustomTextInput.js
@@ -2,34 +2,47 @@ import React from 'react';
 
 import {TextInput} from '../styles/components';
 import {View} from 'react-native-animatable';
-import {Text} from 'react-native';
+import {Text, Platform} from 'react-native';
 // import { Container } from './styles';
+import {TextInputMask} from 'react-native-masked-text'
+
 
 const CustomTextInput = ({
   value,
+  rmask,
   onChangeText,
   placeholder,
   editable,
   secureTextEntry,
 }) => {
   return (
-    <View style={{position: 'relative', margin: 10}}>
-      <Text
+    <View style={{margin: 10}}>
+      {/*<Text
         style={{
           backgroundColor: '#FFF',
-          position: 'absolute',
-          top: -10,
-          left: 20,
-        }}>
+          top: Platform.OS == 'ios' ? 1 :55,
+          left: 20
+          }}
+        >
         {placeholder}
-      </Text>
-      <TextInput
-        // editable={editable ? true : true}
+        </Text>*/}
+      <TextInputMask
+      style={{backgroundColor: '#fff',
+        fontSize: 17,
+        paddingLeft: 10,
+     
+        borderRadius: 7,
+        borderWidth: 0.5,
+        borderColor: '#555',
+        color: '#1f79ad',
+        height: 60,
+      marginVertical: 5, marginHorizontal:10}}
+        type={'custom'}
         value={value}
+        options={{mask: rmask}}
         onChangeText={onChangeText}
-        secureTextEntry={secureTextEntry ? true : false}
-        placeholderTextColor="#77bde5"
       />
+     
     </View>
   );
 };
